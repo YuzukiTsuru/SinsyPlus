36fec65e554ab05b69a30231ecbc9290ef7156f3
740291272 <740291272@qq.com>
2018-11-17 15:08:05 +0800

    add file to git

--- curl.py
+++ curl.py
@@ -3,6 +3,6 @@ import os
 
 # 下载(语言，语言名称，XML文件)
 def curl(lang, langstr, XML):
-    f = os.popen("curl -X POST -F 'SPKR_LANG=" + langstr + "' -F 'SPKR=" + lang +"' -F 'SYNALPHA=0.55' -F 'VIBPOWER=1' -F 'F0SHIFT=0' -F  'SYNSRC=@" + XML + "' http://sinsy.sp.nitech.ac.jp/index.php | grep 'lf0'")
+    f = os.popen("curl/curl.exe -X POST -F 'SPKR_LANG=" + langstr + "' -F 'SPKR=" + lang +"' -F 'SYNALPHA=0.55' -F 'VIBPOWER=1' -F 'F0SHIFT=0' -F  'SYNSRC=@" + XML + "' http://sinsy.sp.nitech.ac.jp/index.php | grep 'lf0'")
     textsrc = f.readlines()
     return textsrc
